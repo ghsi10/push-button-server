e474377bdc1244ff56a9ce35881494d4931b581e
idan <ghsi10@gmail.com>
2019-02-04 22:33:32 +0200

    add development.

--- src/main/java/com/pushbutton/configs/SecurityConfig.java
+++ src/main/java/com/pushbutton/configs/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.pushbutton.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,23 +14,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    @Value("${login.username}")
+    String username;
+    @Value("${login.password}")
+    String password;
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin")
-                .password(passwordEncoder.encode("admin"))
+                .withUser(username)
+                .password(password)
                 .roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .headers().frameOptions().disable()
+                .and()
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/css/**", "/js/**").permitAll()
+                .antMatchers("/register-phone", "/click-button").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
